2a5177849df032fe63c9e17527c4593c305a2259
Marvin4lonso <malonsoa@miumg.edu.gt>
2023-01-21 18:57:32 -0600

    Se agrega el proyecto al repo

---
+++ DXWebApp_EVENTOS/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using DXWebApp_EVENTOS.Models;
+
+namespace DXWebApp_EVENTOS
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DXWebApp_EVENTOS/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="DXWebApp_EVENTOS.Global_asax" Language="C#" %>
