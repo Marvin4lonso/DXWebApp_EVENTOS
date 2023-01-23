788c2a27c3d7d414324f34de3cc64d9e5c4bc6a9
Marvin Alonso <malonsoa@miumg.edu.gt>
2023-01-23 16:47:10 -0600

    Se agregaron controles al pop para nuevo evento

--- DXWebApp_EVENTOS/Root.master.cs
+++ DXWebApp_EVENTOS/Root.master.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNet.Identity;
 namespace DXWebApp_EVENTOS {
     public partial class RootMaster : System.Web.UI.MasterPage {
         protected void Page_Load(object sender, EventArgs e) {
-            ASPxLabel2.Text = DateTime.Now.Year + Server.HtmlDecode(" &copy; Copyright by [company name]");
+            ASPxLabel2.Text = DateTime.Now.Year + Server.HtmlDecode(" &copy; Copyright by IT Broadcast");
         }
         protected void HeadLoginStatus_LoggingOut(object sender, LoginCancelEventArgs e) {
             Context.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
