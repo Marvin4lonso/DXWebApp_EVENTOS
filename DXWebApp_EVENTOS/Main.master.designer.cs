18919f03499ce61d54ea50b1271c0f0551c4482c
Marvin Alonso <malonsoa@miumg.edu.gt>
2023-01-23 17:22:00 -0600

    Se agrega el boton de agregar al popup para nuevo evento

--- DXWebApp_EVENTOS/Main.master.designer.cs
+++ DXWebApp_EVENTOS/Main.master.designer.cs
@@ -148,6 +148,15 @@ namespace DXWebApp_EVENTOS {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
         
         /// <summary>
+        /// dxBtnAgregar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxButton dxBtnAgregar;
+        
+        /// <summary>
         /// dxPopUpEventoModificar control.
         /// </summary>
         /// <remarks>
